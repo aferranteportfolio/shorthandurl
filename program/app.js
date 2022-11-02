996bb953d037f90976b4b629f920882dc4ec7bcc
Adriano <adrianoferrantedepiante48@gmail.com>
2022-11-02 07:35:54 -0500

    added protocolverification and protocolmender functions to verify the protocol state of the urls sent in the request

--- program/app.js
+++ program/app.js
@@ -1,8 +1,7 @@
-import {hello , randomNumbersConcatenation, randomNumberSlicer, getRandom} from '../program/functionality.js'
+import { randomNumbersConcatenation, randomNumberSlicer, getRandom, protocolVerification, protocolMender} from '../program/functionality.js'
 
 let button = document.getElementById('button')
 button.addEventListener('click', ()=> {
-    
     let test = randomNumbersConcatenation()
-    console.log(test)
+    protocolMender()
 })
--- program/functionality.js
+++ program/functionality.js
@@ -1,18 +1,53 @@
-function getRandom(){
+function getRandom() {
     let randomStringNumber = Math.random().toString(32)
     return randomStringNumber
 }
 
-function randomNumberSlicer(string){
+function randomNumberSlicer(string) {
     string = getRandom()
-    return string.substring(2,5)
+    return string.substring(2, 5)
 }
 
-function randomNumbersConcatenation(string){
+function randomNumbersConcatenation(string) {
     string = randomNumberSlicer() + randomNumberSlicer()
+    
     return string
 }
 
 
-export { randomNumbersConcatenation, randomNumberSlicer, getRandom}
+function getUrl() {
+    let inputBox = document.getElementById('inputbox').value
+    return inputBox
+}
 
+
+
+function protocolVerification(string) {
+    string = 'https://www.youtube.com/watch?v=SlYcnGW0NEg&list=RDSlYcnGW0NEg&start_radio=1&ab_channel=TitoSakuraba'
+    let protocolVerification = string.startsWith("http://") || string.startsWith("https://") || string.startsWith("ftp://")
+    return protocolVerification
+}
+
+function protocolMender(){
+    let newurl
+    if (!protocolVerification()){
+        newurl = "http://"+protocolVerification()
+       
+        return newurl
+       
+    } else
+    console.log('the Url submitted has the protocol')
+    return newurl
+
+}
+
+
+
+export { randomNumbersConcatenation, randomNumberSlicer, getRandom, protocolVerification, protocolMender }
+
+
+
+
+
+// function geturl(){     var url = document.getElementById(“urlinput”).value;
+// if(!protocol_ok){         newurl = “http://”+url;         return newurl;     }else{         return url;     }
